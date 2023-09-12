1dd713d10d03af3c5f4422095944fb7de9dc2c9d
Aidan <aidanthomas64@gmail.com>
2023-09-12 00:50:04 -0700

    still testing

--- about/script.js
+++ about/script.js
@@ -1,14 +1,41 @@
-const textarea = document.getElementById('textarea');
-const status = document.getElementById('status');
-const mmbutton = document.getElementById('mmbutton');
-
-mmbutton.addEventListener("click", buttonPressSend);
-
-// function buttonPressSend() {
-//     const textVar = textarea.value;
-//     textarea.textContent = textVar;
-//     alert('a');
-// }
+const mButton = document.getElementById('mbutton');
+const mStatus = document.getElementById('mstatus');
+mButton.addEventListener("click", mButtonClicked);
 
 
-alert('helo');
+function mButtonClicked() {
+    var x = contactServer();
+}
+
+function contactServer() {
+    mStatus.textContent = "sent";
+
+
+    const url = '/script.py';
+    const data = { key1: 'value1', key2: 'value2' };
+
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            // Add any other headers your API requires here
+        },
+        body: JSON.stringify(data), // Convert data to JSON string
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json(); // Parse response body as JSON
+        })
+        .then(data => {
+            console.log('PUT request succeeded with JSON response:', data);
+        })
+        .catch(error => {
+            console.error('PUT request failed:', error);
+        });
+
+
+    mStatus.textContent = "recieved";
+}
+
---
+++ script.py
@@ -0,0 +1,10 @@
+#! /bin/python3
+
+#!/usr/bin/python3
+print ("Content-type: text/html \r\n")
+print ('''<html>
+<head><title>My first Python CGI app</title></head>
+<body>
+<p>Hello, 'world'!</p>
+</body>
+</html>''')
