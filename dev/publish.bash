82c89bc0ec1db437c51ff2b0dcfff98fe687f3e8
Aidan Thomas <126053853+aidant64@users.noreply.github.com>
2023-09-28 19:18:54 -0700

    Update publish.bash

--- dev/publish.bash
+++ dev/publish.bash
@@ -8,4 +8,4 @@ sudo rm -rf personal_website/
 
 sudo chmod 777 /var/www/html/responses
 sudo chmod 777 /var/www/html/responses/incrementer
-sudo rm -rf /var/www/html/dev
+sudo rm -rf /var/www/html/dev /var/www/html/README.md
