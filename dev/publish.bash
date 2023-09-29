d8e305ce2766313c2b2d20d1083916e260455640
Aidan <aidanthomas64@gmail.com>
2023-09-28 21:37:44 -0700

    updated publish.bash and fixed =banner horizontal scrolling on mobile

--- dev/publish.bash
+++ dev/publish.bash
@@ -3,6 +3,7 @@
 mkdir ~/tmp_salt2309
 cp -r ~/personal_website/* ~/tmp_salt2309
 rm -rf ~/tmp_salt2309/dev ~/tmp_salt2309/README.md
+ssh aws mkdir personal_website
 scp -r ~/tmp_salt2309/* aws:/home/ubuntu/personal_website/
 rm -rf ~/tmp_salt2309
 
