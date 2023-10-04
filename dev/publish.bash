adad25cbc1c9fff8719132d7dbd0446ee2b8a390
Aidan <aidanthomas64@gmail.com>
2023-10-03 23:53:43 -0700

    better publish bssh script, changed hover disable

--- dev/publish.bash
+++ dev/publish.bash
@@ -2,9 +2,10 @@
 
 mkdir ~/tmp_salt2309
 cp -r ~/personal_website/* ~/tmp_salt2309
-rm -rf ~/tmp_salt2309/dev ~/tmp_salt2309/README.md
-ssh aws mkdir personal_website
+rm -rf ~/tmp_salt2309/dev ~/tmp_salt2309/README.md ~/tmp_salt2309/poll/data
+
+ssh aws "rm -rf personal_website && mkdir personal_website"
 scp -r ~/tmp_salt2309/* aws:/home/ubuntu/personal_website/
 rm -rf ~/tmp_salt2309
 
-ssh aws "sudo rm -rf /var/www/html/*i && sudo cp -r personal_website/* /var/www/html/ && sudo rm -rf personal_website/ && sudo chmod 777 /var/www/html/poll/data"
+ssh aws "sudo cp /var/www/html/poll/data personal_website/poll/ && sudo rm -rf /var/www/html/*i && sudo cp -r personal_website/* /var/www/html/ && sudo rm -rf personal_website/ && sudo chmod 777 /var/www/html/poll/data"
