3c2acc5ee27ef9dac8345dc5d948ab1aee39e880
Aidan <aidanthomas64@gmail.com>
2023-12-16 19:31:17 -0800

    populated buoys califonia+hawaii

--- waveapp/map.js
+++ waveapp/map.js
@@ -87,7 +87,220 @@ async function initMap() {
         {
             position: { lat: 35.371, lng: -120.859 },
             title: "MBXC1",
+        },
+        {
+            position: { lat: 33.860, lng: -118.641 },
+            title: "46221",
+        },
+        {
+            position: { lat: 34.022, lng: -118.578 },
+            title: "46268",
+        },
+        {
+            position: { lat: 33.618, lng: -118.317 },
+            title: "46222",
+        },
+        {
+            position: { lat: 33.576, lng: -118.181 },
+            title: "46253",
+        },
+        {
+            position: { lat: 33.700, lng: -118.201 },
+            title: "46256",
+        },
+        {
+            position: { lat: 33.733, lng: -118.186 },
+            title: "PRJC1",
+        },
+        {
+            position: { lat: 33.716, lng: -118.246 },
+            title: "AGXC1",
+        },
+        {
+            position: { lat: 32.388, lng: -119.525 },
+            title: "46047",
+        },
+        {
+            position: { lat: 32.499, lng: -118.052 },
+            title: "46086",
+        },
+        {
+            position: { lat: 32.517, lng: -117.425 },
+            title: "46232",
+        },
+        {
+            position: { lat: 32.570, lng: -117.169 },
+            title: "46235",
+        },
+        {
+            position: { lat: 32.575, lng: -117.127 },
+            title: "TIXC1",
+        },
+        {
+            position: { lat: 32.714, lng: -117.174 },
+            title: "SDBC1",
+        },
+        {
+            position: { lat: 32.749, lng: -117.502 },
+            title: "46258",
+        },
+        {
+            position: { lat: 32.868, lng: -117.267 },
+            title: "46254",
+        },
+        {
+            position: { lat: 32.933, lng: -117.391 },
+            title: "46225",
+        },
+        {
+            position: { lat: 32.957, lng: -117.279 },
+            title: "46266",
+        },
+        {
+            position: { lat: 33.062, lng: -117.314 },
+            title: "46274",
+        },
+        {
+            position: { lat: 33.178, lng: -117.472 },
+            title: "46224",
+        },
+        {
+            position: { lat: 33.290, lng: -117.5 },
+            title: "46275",
+        },
+        {
+            position: { lat: 33.336, lng: -117.659 },
+            title: "46277",
+        },
+        {
+            position: { lat: 40.764, lng: -137.377 },
+            title: "46006",
+        },
+        {
+            position: { lat: 38.069, lng: -129.976 },
+            title: "46059",
+        },
+        {
+            position: { lat: 39.335, lng: -127.070 },
+            title: "46411",
+        },
+        {
+            position: { lat: 35.170, lng: -120.741 },
+            title: "CPXC1",
+        },
+        {
+            position: { lat: 35.169, lng: -120.754 },
+            title: "PSLC1",
+        },
+        {
+            position: { lat: 35.204, lng: -120.859 },
+            title: "46215",
+        },
+        {
+            position: { lat: 35.770, lng: -121.903 },
+            title: "46028",
+        },
+        {
+            position: { lat: 36.335, lng: -122.104 },
+            title: "46239",
+        },
+        {
+            position: { lat: 36.626, lng: -121.907 },
+            title: "46240",
+        },
+        {
+            position: { lat: 36.838, lng: -121.82 },
+            title: "46279",
+        },
+        {
+            position: { lat: 36.845, lng: -121.825 },
+            title: "46276",
+        },
+        {
+            position: { lat: 37.35, lng: -122.88 },
+            title: "1801589",
+        },
+        {
+            position: { lat: 37.754, lng: -122.839 },
+            title: "46026",
+        },
+        {
+            position: { lat: 37.788, lng: -122.634 },
+            title: "46237",
+        },
+        {
+            position: { lat: 37.806, lng: -122.466 },
+            title: "FTPC1",
+        },
+        {
+            position: { lat: 37.923, lng: -122.41 },
+            title: "RCMC1",
+        },
+        {
+            position: { lat: 37.937, lng: -123.463 },
+            title: "46214",
+        },
+        {
+            position: { lat: 38.235, lng: -123.317 },
+            title: "46013",
+        },
+        {
+            position: { lat: 39.225, lng: -123.98 },
+            title: "46014",
+        },
+        {
+            position: { lat: 40.295, lng: -124.732 },
+            title: "46213",
+        },
+        {
+            position: { lat: 40.713, lng: -124.54 },
+            title: "46022",
+        },
+        {
+            position: { lat: 40.769, lng: -124.239 },
+            title: "NJLC1",
+        },
+        {
+            position: { lat: 40.896, lng: -124.357 },
+            title: "46244",
+        },
+        {
+            position: { lat: 41.840, lng: -124.382 },
+            title: "46027",
+        },
+        {
+            position: { lat: 46.143, lng: -131.09 },
+            title: "46005",
+        },
+        {
+            position: { lat: 48.360, lng: -133.94 },
+            title: "46036",
+        },
+        {
+            position: { lat: 50.055, lng: -144.873 },
+            title: "48400",
+        },
+        {
+            position: { lat: 23.528, lng: -153.792 },
+            title: "51000",
+        },
+        {
+            position: { lat: 24.451, lng: -162.008 },
+            title: "51001",
+        },
+        {
+            position: { lat: 24.359, lng: -162.081 },
+            title: "51101",
+        },
+        {
+            position: { lat: 19.196, lng: -160.639 },
+            title: "51003",
+        },
+        {
+            position: { lat: 17.042, lng: -157.746 },
+            title: "51002",
         }
+
 
     ];
 
@@ -96,22 +309,12 @@ async function initMap() {
     //     title: "",
     // }
 
-    const diamondSymbol = {
-        path: google.maps.SymbolPath.CIRCLE,
-        fillColor: 'red',
-        fillOpacity: 1,
-        strokeColor: 'white',
-        strokeWeight: 1,
-        scale: 10,
-    };
-
     //Create the markers.
     locations.forEach(({ position, title }, i) => {
         const marker = new google.maps.Marker({
             position,
             map,
             title: `${title}`,
-            icon: diamondSymbol,
         });
 
         // Add a click listener for each marker, and set up the info window.
@@ -163,7 +366,7 @@ function printKeyValuePairs(jsonObject) {
 
     for (const key in jsonObject) {
         if (Object.hasOwnProperty.call(jsonObject, key)) {
-            result += `${key}: ${jsonObject[key]}<br><br>`;
+            result += `${key}: ${jsonObject[key]}<br>`;
         }
     }
 
