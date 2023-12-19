0700639a647b8b32be9411bdfa425e0e8cdc0052
Aidan <aidanthomas64@gmail.com>
2023-12-19 02:50:21 -0800

    fixed dev/publish bash script

--- dev/publish.bash
+++ dev/publish.bash
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-tar --exclude='.git' -czf - /home/aidan/personal_website | ssh aws 'cd /home/ubuntu/ && tar -xzf -'
-#ssh aws "sudo rm -rf /var/www/website;  sudo mv /home/ubuntu/personal_website /var/www/website && sudo chown -R www-data:www-data /var/www/website"
+tar --exclude='.git' -czf - /home/aidan/personal_website | ssh aws 'cd /home/ubuntu/ && tar -xzf - && mv /home/ubuntu/home/aidan/personal_website /home/ubuntu/ && rm -rf /home/ubuntu/home/'
+ssh aws "sudo rm -rf /var/www/website;  sudo mv /home/ubuntu/personal_website /var/www/website && sudo chown -R www-data:www-data /var/www/website"
 
