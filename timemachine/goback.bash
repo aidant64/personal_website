2a6a7e67ec5e3a3fc44a473cf5020b1f073b39e8
Aidan <aidanthomas64@gmail.com>
2023-12-20 01:56:46 -0800

    working on timemachine pt2

--- timemachine/goback.bash
+++ timemachine/goback.bash
@@ -1,8 +1,14 @@
 #! /bin/bash
-echo "Conetent-Type: html/html"
-echo ""
-echo "dog"
 
-cd /var/www/archive && git checkout 3acc3e8
+#cd /var/www/archive && git checkout -f  eb34fa1
+query_string="$QUERY_STRING"
+param1=$(echo "$query_string" | awk -F'&' '{split($1, arr, "="); print arr[2]}')
 
-echo "cat"
+cd /var/www/archive && git checkout -f $param1
+
+printf "Status: 302 Found\n"
+printf "Location: http://archive.aidanswesbite.com/\n"
+printf "Content-Type: text/html\n\n"
+
+# Optionally, you can include an HTML message for browsers that don't follow the redirect automatically
+printf "<html><body>Redirecting to <a href=\"http://archive.aidanswesbite.com/\">http://archive.aidanswesbite.com/</a></body></html>\n"
--- timemachine/index.php
+++ timemachine/index.php
@@ -5,16 +5,111 @@
 <html>
 
 <head>
-    <title>Hello, World!</title>
+    <title>Commit History</title>
 </head>
 
 <body>
-    <h1>Hello, World!</h1>
-    <p>This is a basic HTML "Hello, World!" example.</p>
-
-    <button onclick="getRequest()">TIME</button>
-
-    <script src='/timemachine/script.js' defer></script>
+    <h1>Commit History Links</h1>
+    <ul>
+        <li><a href="/timemachine/goback.bash?hash=a433ff9">working on timemachine</a></li>
+        <li><a href="/timemachine/goback.bash?hash=b2a7501">pretty kind of</a></li>
+        <li><a href="/timemachine/goback.bash?hash=1842d8e">added colors</a></li>
+        <li><a href="/timemachine/goback.bash?hash=15e1903">simplified directories and htacess rules</a></li>
+        <li><a href="/timemachine/goback.bash?hash=6687eb7">futre proofing</a></li>
+        <li><a href="/timemachine/goback.bash?hash=018cf44">rewrite rules in htaccess for simple folders</a></li>
+        <li><a href="/timemachine/goback.bash?hash=0700639">fixed dev/publish bash script</a></li>
+        <li><a href="/timemachine/goback.bash?hash=9b903e1">CHANGED EVERYTHING</a></li>
+        <li><a href="/timemachine/goback.bash?hash=7606ad0">removed duplicate buoy</a></li>
+        <li><a href="/timemachine/goback.bash?hash=ba76c4f">added marshal islands buoys</a></li>
+        <li><a href="/timemachine/goback.bash?hash=b698ec9">last change, before removing PHP backend</a></li>
+        <li><a href="/timemachine/goback.bash?hash=38fef76">made executable</a></li>
+        <li><a href="/timemachine/goback.bash?hash=85722ef">modify donwload button style</a></li>
+        <li><a href="/timemachine/goback.bash?hash=1713a7d">added download JAR button</a></li>
+        <li><a href="/timemachine/goback.bash?hash=1bb6d9e">made translation more clear to user</a></li>
+        <li><a href="/timemachine/goback.bash?hash=21f2863">removed unicode emoji because of sizing issue</a></li>
+        <li><a href="/timemachine/goback.bash?hash=b9601cd">changed unicode icons</a></li>
+        <li><a href="/timemachine/goback.bash?hash=3ff721d">added waveapp link in menu tabs</a></li>
+        <li><a href="/timemachine/goback.bash?hash=ad2077c">more icons</a></li>
+        <li><a href="/timemachine/goback.bash?hash=3c2acc5">populated buoys califonia+hawaii</a></li>
+        <li><a href="/timemachine/goback.bash?hash=a870c58">created sub website /map for weather</a></li>
+        <li><a href="/timemachine/goback.bash?hash=2bc2b26">finished synch w server</a></li>
+        <li><a href="/timemachine/goback.bash?hash=c9e9b71">synch with server</a></li>
+        <li><a href="/timemachine/goback.bash?hash=a5557ad">fixed bad timezone bug fix, added loc names, print only 1
+                decimal</a></li>
+        <li><a href="/timemachine/goback.bash?hash=663d9b5">second timezone bug fix</a></li>
+        <li><a href="/timemachine/goback.bash?hash=9a9f22d">weather timezone bug fixed, added loading ui pretty</a></li>
+        <li><a href="/timemachine/goback.bash?hash=788b75a">WEATHER</a></li>
+        <li><a href="/timemachine/goback.bash?hash=c9330e4">unicode, utf-8</a></li>
+        <li><a href="/timemachine/goback.bash?hash=f82bf55">changed icon size for mobile and desktop</a></li>
+        <li><a href="/timemachine/goback.bash?hash=7bba058">typo</a></li>
+        <li><a href="/timemachine/goback.bash?hash=39679ef">changed document root</a></li>
+        <li><a href="/timemachine/goback.bash?hash=2bc58b3">light theme</a></li>
+        <li><a href="/timemachine/goback.bash?hash=77c86c0">personal prefference changes</a></li>
+        <li><a href="/timemachine/goback.bash?hash=1660d41">added country icon</a></li>
+        <li><a href="/timemachine/goback.bash?hash=8825ce9">bug fixes, user help</a></li>
+        <li><a href="/timemachine/goback.bash?hash=6869a99">added router.php</a></li>
+        <li><a href="/timemachine/goback.bash?hash=f94158f">images</a></li>
+        <li><a href="/timemachine/goback.bash?hash=073a28d">bunch of stuff</a></li>
+        <li><a href="/timemachine/goback.bash?hash=615133e">forced cached contenet to update</a></li>
+        <li><a href="/timemachine/goback.bash?hash=fc2e0cf">Update README.md</a></li>
+        <li><a href="/timemachine/goback.bash?hash=676f492">Update README.md</a></li>
+        <li><a href="/timemachine/goback.bash?hash=03f4946">Update README.md</a></li>
+        <li><a href="/timemachine/goback.bash?hash=af3b178">Update README.md</a></li>
+        <li><a href="/timemachine/goback.bash?hash=57cfdd5">small changes</a></li>
+        <li><a href="/timemachine/goback.bash?hash=a28d801">changed translation accuracy by keeping newlines</a></li>
+        <li><a href="/timemachine/goback.bash?hash=c41097d">fix title bug</a></li>
+        <li><a href="/timemachine/goback.bash?hash=eb34fa1">css fix</a></li>
+        <li><a href="/timemachine/goback.bash?hash=3d5fa82">refined languages, new delimeter, error checking</a></li>
+        <li><a href="/timemachine/goback.bash?hash=54f78a7">language support, removed alert</a></li>
+        <li><a href="/timemachine/goback.bash?hash=de7e73e">translations continue</a></li>
+        <li><a href="/timemachine/goback.bash?hash=8925d56">multi language support alpha</a></li>
+        <li><a href="/timemachine/goback.bash?hash=6907afa">added php</a></li>
+        <li><a href="/timemachine/goback.bash?hash=bf1a7a3">added php</a></li>
+        <li><a href="/timemachine/goback.bash?hash=1801bf6">Update conf</a></li>
+        <li><a href="/timemachine/goback.bash?hash=df59bc5">Update conf</a></li>
+        <li><a href="/timemachine/goback.bash?hash=41848ef">Update conf for ip</a></li>
+        <li><a href="/timemachine/goback.bash?hash=344cf07">added apache2 conf file, removed fancy font</a></li>
+        <li><a href="/timemachine/goback.bash?hash=a062617">added google analytics and notification</a></li>
+        <li><a href="/timemachine/goback.bash?hash=62c8f24">removed cookies, fixed bugs</a></li>
+        <li><a href="/timemachine/goback.bash?hash=a615b19">added transition</a></li>
+        <li><a href="/timemachine/goback.bash?hash=b222785">fix bug</a></li>
+        <li><a href="/timemachine/goback.bash?hash=adad25c">better publish bssh script, changed hover disable</a></li>
+        <li><a href="/timemachine/goback.bash?hash=8082bc6">used js for checking mobile devices</a></li>
+        <li><a href="/timemachine/goback.bash?hash=3acc3e8">title change</a></li>
+        <li><a href="/timemachine/goback.bash?hash=d1e9cec">horizontal graph</a></li>
+        <li><a href="/timemachine/goback.bash?hash=4f6b265">fixed bugs</a></li>
+        <li><a href="/timemachine/goback.bash?hash=bc568b6">big change; added viewer poll, removed messaging</a></li>
+        <li><a href="/timemachine/goback.bash?hash=e9bed1">sec</a></li>
+        <li><a href="/timemachine/goback.bash?hash=6a27392">title change</a></li>
+        <li><a href="/timemachine/goback.bash?hash=7bff1c2">misc</a></li>
+        <li><a href="/timemachine/goback.bash?hash=a910134">changed github url</a></li>
+        <li><a href="/timemachine/goback.bash?hash=7599fe2">resized images for mobile</a></li>
+        <li><a href="/timemachine/goback.bash?hash=d8e305c">updated publish.bash and fixed =banner horizontal scrolling
+                on mobile</a></li>
+        <li><a href="/timemachine/goback.bash?hash=2df92bf">made executable</a></li>
+        <li><a href="/timemachine/goback.bash?hash=be3a30d">Delete dev/local-publish.bash</a></li>
+        <li><a href="/timemachine/goback.bash?hash=e80fd79">Update publish.bash</a></li>
+        <li><a href="/timemachine/goback.bash?hash=21a8bc8">Update publish.bash</a></li>
+        <li><a href="/timemachine/goback.bash?hash=9fecfd6">Create local-publish.bash</a></li>
+        <li><a href="/timemachine/goback.bash?hash=82c89bc">Update publish.bash</a></li>
+        <li><a href="/timemachine/goback.bash?hash=86acd55">Update publish.bash</a></li>
+        <li><a href="/timemachine/goback.bash?hash=ae4b48f">Create publish.bash</a></li>
+        <li><a href="/timemachine/goback.bash?hash=ff83a5b">mobile formatting in css</a></li>
+        <li><a href="/timemachine/goback.bash?hash=e41f3e7">server pull</a></li>
+        <li><a href="/timemachine/goback.bash?hash=aafe6de">keep up with server</a></li>
+        <li><a href="/timemachine/goback.bash?hash=b2d4488">non-published changes</a></li>
+        <li><a href="/timemachine/goback.bash?hash=43bee37">pulled from server</a></li>
+        <li><a href="/timemachine/goback.bash?hash=3cb6ba9">Update README.md</a></li>
+        <li><a href="/timemachine/goback.bash?hash=5f8bc40">Update README.md</a></li>
+        <li><a href="/timemachine/goback.bash?hash=3490fea">new github URL, limited to 3 messages</a></li>
+        <li><a href="/timemachine/goback.bash?hash=6057f50">Update projects.html - include correct github link</a></li>
+        <li><a href="/timemachine/goback.bash?hash=f3e3736">Delete projects/style.css</a></li>
+        <li><a href="/timemachine/goback.bash?hash=9d90352">Update README.md</a></li>
+        <li><a href="/timemachine/goback.bash?hash=f654f4e">Update README.md</a></li>
+        <li><a href="/timemachine/goback.bash?hash=ee21a76">Update README.md</a></li>
+        <li><a href="/timemachine/goback.bash?hash=1500306">second server version</a></li>
+        <li><a href="/timemachine/goback.bash?hash=69214a9">server version</a></li>
+    </ul>
 </body>
 
 </html>
